f460bc3fdbd3f478930682eb6040a018ba195f18
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 21:24:35 +0000

    Automatically backed up by Learn

--- lib/scrapernotes.rb
+++ lib/scrapernotes.rb
@@ -31,3 +31,13 @@ puts doc.css(".headline-26OIBN")
 #.text and .strip: and get only the text contained inside the element that we want. Alternatively, using p will produce the array-like object we saw from before.
 
 #We did it! We used Nokogiri to get the HTML of a web page. We used the element inspector in the browser to ID the CSS selector of the data we wanted to scrape. We used the .css Nokogiri method, along with that CSS selector, to grab the element that contains our desired data. Finally, we used the .text method to retrieve the desired text.
+
+
+#Iterating over elements: 
+require 'nokogiri'
+require 'open-uri'
+
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
