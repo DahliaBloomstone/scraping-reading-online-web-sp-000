481c1c23c150e1da35d99132e3df7c2400a74855
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 21:13:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
+#We can use the following line to grab the HTML that makes up the Flatiron School's landing page at flatironschool.com:
 html = open("https://flatironschool.com/")
 
+#method to take the string of HTML returned by open-uri's open method and convert it into a NodeSet
 Nokogiri::HTML(html)
+
+#Let's save the HTML document in a variable, doc that we can then operate on:
+
 
 doc = Nokogiri::HTML(html)
