7b8fa70d265c56fc0ba2a2a7133b5b0677d6f6d4
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-04 21:14:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,3 +9,6 @@ Nokogiri::HTML(html)
 
 #Let's save the HTML document in a variable, doc that we can then operate on:
 doc = Nokogiri::HTML(html)
+
+#puts doc
+#What we're looking at here is all of the HTML that makes up the web page found at www.flatironschool.com. The massive lines above are actually a snapshot of that HTML converted into a structure of nested nodes by Nokogiri.
